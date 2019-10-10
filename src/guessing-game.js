bf9f05a3eeeffc0e8fbe7c2cc46d4790243b3b58
VladislavRb <vlad.raba@mail.ru>
2019-10-10 11:32:28 +0300

    fix: finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this._start;
+        this._end;
+        this._suggestion;
+    }
 
     setRange(min, max) {
-
+        this._start = min;
+        this._end = max;
     }
 
     guess() {
-
+        this._suggestion = Math.round((this._start + this._end) / 2);
+        return this._suggestion;
     }
 
     lower() {
-
+        this.setRange(this._start, this._suggestion);
     }
 
     greater() {
-
+        this.setRange(this._suggestion, this._end);
     }
 }
 
